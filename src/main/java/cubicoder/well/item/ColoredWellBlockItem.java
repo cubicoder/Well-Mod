39ca29b7c476e68df2bc411df7c329c6729d4f6f
cubicoder <40398183+cubicoder@users.noreply.github.com>
2024-07-11 22:24:27 -0400

    Fixed dyed wells clearing dye in powdered snow cauldrons

--- src/main/java/cubicoder/well/item/ColoredWellBlockItem.java
+++ src/main/java/cubicoder/well/item/ColoredWellBlockItem.java
@@ -10,6 +10,9 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LayeredCauldronBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluids;
+import net.neoforged.neoforge.capabilities.Capabilities;
+import net.neoforged.neoforge.fluids.capability.IFluidHandler;
 import net.neoforged.neoforge.items.ItemHandlerHelper;
 
 public class ColoredWellBlockItem extends BlockItem {
@@ -17,26 +20,29 @@ public class ColoredWellBlockItem extends BlockItem {
 	public ColoredWellBlockItem(Block block, Properties properties) {
 		super(block, properties);
 	}
-
+	
 	@Override
 	public InteractionResult useOn(UseOnContext context) {
 		Level level = context.getLevel();
 		Player player = context.getPlayer();
 		BlockState state = level.getBlockState(context.getClickedPos());
 		if (state.getBlock() instanceof LayeredCauldronBlock) {
-			if (!level.isClientSide && !player.isCreative()) {
-				int cauldronLevel = state.getValue(LayeredCauldronBlock.LEVEL);
-				if (cauldronLevel > 0) {
-					player.getItemInHand(context.getHand()).shrink(1);
-					player.awardStat(Stats.USE_CAULDRON);
-					ItemHandlerHelper.giveItemToPlayer(player, new ItemStack(ModItems.WELL.get()));
-					LayeredCauldronBlock.lowerFillLevel(state, level, context.getClickedPos());
+			// get cauldron fluid
+			IFluidHandler cauldronHandler = level.getCapability(Capabilities.FluidHandler.BLOCK, context.getClickedPos(), state, null, context.getClickedFace());
+			if (cauldronHandler != null && cauldronHandler.getFluidInTank(0).getFluid().isSame(Fluids.WATER)) {
+				if (!level.isClientSide()) {
+					int cauldronLevel = state.getValue(LayeredCauldronBlock.LEVEL);
+					if (cauldronLevel > 0) {
+						assert player != null;
+						player.getItemInHand(context.getHand()).shrink(1);
+						player.awardStat(Stats.USE_CAULDRON);
+						ItemHandlerHelper.giveItemToPlayer(player, new ItemStack(ModItems.WELL.get()));
+						LayeredCauldronBlock.lowerFillLevel(state, level, context.getClickedPos());
+					}
 				}
+				return InteractionResult.sidedSuccess(level.isClientSide());
 			}
-			
-			return InteractionResult.SUCCESS;
 		}
-		
 		return super.useOn(context);
 	}
 	
