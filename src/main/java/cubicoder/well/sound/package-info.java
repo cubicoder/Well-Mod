21c23e2dac2ffe4e7f5ce78e2cd925d24e6a51ab
cubicoder <40398183+cubicoder@users.noreply.github.com>
2024-07-11 21:08:36 -0400

    Add package-info files for default annotations

---
+++ src/main/java/cubicoder/well/sound/package-info.java
@@ -0,0 +1,9 @@
+@FieldsAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package cubicoder.well.sound;
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
